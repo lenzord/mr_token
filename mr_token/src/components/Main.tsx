6be468d09e4a841a028e6f368317351c6e9af31c
lenz <lenzordmusic@gmail.com>
2022-07-25 10:30:51 -0400

    feat: implement card with steady progress bar

--- mr_token/src/components/Main.tsx
+++ mr_token/src/components/Main.tsx
@@ -9,6 +9,7 @@ function MainComponent() {
           <a href="#home">HOME</a>
           <a href="market">MARKET</a>
           <a href="staking">STAKING</a>
+          <a href="buy">BUY TOKEN</a>
         </div>
       </div>
       <div className="main-content">
@@ -26,9 +27,49 @@ function MainComponent() {
           Compre agora!
           </h4>
         </div>
+        <div className="paper-button">
+          <a href="w-paper">WHITE PAPER</a>
+          <a href="b-paper">LIGHT PAPER</a>
+        </div>
+      </div>
+      <div className="card-info">
+        <div className="card-first-row">
+          <div className="card-info-logo">Sample img</div>
+          <div className="card-sale-price">
+            <span>NFT-SALE</span>
+            <p>Price:U$199</p>
+          </div>
+          <div className="card-sale-time">
+            <span>SALE STARTED</span>
+            <p>Ends in 90 days</p>
+          </div>
+        </div>
+        <div className="card-second-row">
+          <span>
+            NFTS RAISED /100
+            <p>BUSD</p>
+          </span>
+          <span>
+            MINIMUM ALLOCATION
+            <p>160.000 BUSD</p>
+          </span>
+          <span>
+            MAXIMUM ALLOCATION
+            <p>200.000 BUSD</p>
+          </span>
+        </div>
+        <div className="card-third-row">
+          <div className="progress">
+            <div className="progress__fill"></div>
+            <span className="progress__text">50%</span>
+          </div>
+        </div>
+      </div>
+      <div className="page-footer">
+        <footer/>
       </div>
     </div>
   );
 }
 
-export default MainComponent;
+export default MainComponent
